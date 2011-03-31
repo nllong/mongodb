8fc48099bb15bf10aa41d3c09b14f4ad4184c247
Muerdter, Nick <nick.muerdter@nrel.gov>
2011-03-31 02:01:30 +0000

    Fixes to create the mongodb data dir in another location. Start the mongodb service.

--- attributes/default.rb
+++ attributes/default.rb
@@ -8,11 +8,11 @@
 #
 
 if platform?("redhat", "centos", "fedora")
-  set[:mongodb][:server][:config_file] = "/etc/mongod.conf"
-  set[:mongodb][:server][:log_file] = "/var/log/mongo/mongod.log"
-  set[:mongodb][:server][:db_dir] = "/var/lib/mongo"
+  default[:mongodb][:server][:config_file] = "/etc/mongod.conf"
+  default[:mongodb][:server][:log_file] = "/var/log/mongo/mongod.log"
+  default[:mongodb][:server][:db_dir] = "/var/lib/mongo"
 else
-  set[:mongodb][:server][:config_file] = "/etc/mongodb.conf"
-  set[:mongodb][:server][:log_file] = "/var/log/mongodb/mongodb.log"
-  set[:mongodb][:server][:db_dir] = "/var/lib/mongodb"
+  default[:mongodb][:server][:config_file] = "/etc/mongodb.conf"
+  default[:mongodb][:server][:log_file] = "/var/log/mongodb/mongodb.log"
+  default[:mongodb][:server][:db_dir] = "/var/lib/mongodb"
 end
--- recipes/server.rb
+++ recipes/server.rb
@@ -11,7 +11,14 @@ include_recipe "yum::10gen"
 
 package "mongo-10gen-server"
 
-template node[:mongodb][:server][:config_path] do
+directory node[:mongodb][:server][:db_dir] do
+  owner "mongod"
+  group "mongod"
+  mode "0755"
+  recursive true
+end
+
+template node[:mongodb][:server][:config_file] do
   source "mongodb.conf.erb"
   owner "root"
   group "root"
@@ -27,4 +34,13 @@ if platform?("redhat", "centos", "fedora")
     mode "0644"
     notifies :restart, "service[mongodb]"
   end
+end
+
+service "mongodb" do
+  if platform?("redhat", "centos", "fedora")
+    service_name "mongod"
+  end
+
+  supports :status => true, :restart => true, :reload => true
+  action [:enable, :start]
 end
